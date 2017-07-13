d16cdc803bf98e438231c9e958389bb670b4f085
IgorR <ryabtsev_i@mail.ru>
2017-07-13 21:11:18 +0300

    add Settings to google cloud 5

--- src/main/java/com/realsport/vk/initVk.java
+++ src/main/java/com/realsport/vk/initVk.java
@@ -3,10 +3,7 @@ package com.realsport.vk;
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
-import com.vk.api.sdk.objects.GroupAuthResponse;
 import org.springframework.stereotype.Component;
 
 /**
