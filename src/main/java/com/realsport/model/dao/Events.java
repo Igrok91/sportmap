07bdc09e8c9476d6b8898d8ded213d48a5494f02
Igor <ryabtsev_i@mail.ru>
2018-04-07 21:35:26 +0300

    Версия 3.4

--- src/main/java/com/realsport/model/dao/Events.java
+++ src/main/java/com/realsport/model/dao/Events.java
@@ -2,9 +2,13 @@ package com.realsport.model.dao;
 
 import com.google.cloud.datastore.*;
 import com.realsport.model.entityDao.Event;
+import com.realsport.model.entityDao.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.realsport.model.dao.Persistence.getDatastore;
 import static com.realsport.model.dao.Persistence.getKeyFactory;
@@ -23,16 +27,31 @@ public class Events {
     public void publishEvent(Event game) {
         Transaction tx = getDatastore().newTransaction();
         try {
+            List<EntityValue> list = new ArrayList<>();
+            for (User user: game.getUserList()) {
+                FullEntity userEntity = FullEntity.newBuilder(keyFactory.newKey(user.getUserId()))
+                        .set("firstName", "firstName")
+                        .build();
+                EntityValue value = EntityValue.of(userEntity);
+                list.add(value);
+            }
             FullEntity task = FullEntity.newBuilder(keyFactory.newKey())
                     .set("description", StringValue.newBuilder(game.getDescription()).setExcludeFromIndexes(true).build())
                     .set("userIdCreator", game.getUserIdCreator())
                     .set("playgroundName", game.getPlaygroundName())
                     .set("playgroundId", game.getPlaygroundId())
+                    .set("userFirtsNameCreator", game.getUserFirtsNameCreator())
+                    .set("userLastNameCreator", game.getUserLastNameCreator())
+                    .set("maxCountAnswer", game.getMaxCountAnswer())
+                    .set("duration", game.getDuration())
+                    .set("sport", game.getSport())
+                    .set("active", game.isActive())
+                    .set("dateCreation", String.valueOf(game.getDateCreation()))
+                    .set("userList", ListValue.of(list))
                     .build();
             Entity entity = tx.add(task);
             game.setIdEvent(entity.getKey().getId().toString());
-            System.out.println("IdEvent" +game.getIdEvent());
-            logger.info("IdEvent" + game.getIdEvent());
+            logger.info(" Создание события с Id" + game.getIdEvent());
             tx.commit();
         } finally {
             if (tx.isActive()) {
